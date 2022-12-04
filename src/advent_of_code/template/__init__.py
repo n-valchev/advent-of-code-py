e0b26d51bd546ccdeb914685ff756fd58b6198e6
Nikolay Valchev <niki@st6.io>
2022-12-04 09:05:30 +0200

    use common parse_input fn for both parts of a day challenge

--- src/advent_of_code/template/__init__.py
+++ src/advent_of_code/template/__init__.py
@@ -1,9 +1,15 @@
-def run(input):
-    part_1(input)
-    part_2(input)
+def parse_input(input):
+    return [line.rstrip() for line in input.readlines()]
 
-def part_1(input):
-    print(f'Part_1: {input}')
 
-def part_2(input):
-    print(f'Part_2: {input}')
+def run(file):
+    data = parse_input(file)
+
+    part_1(data)
+    part_2(data)
+
+def part_1(data):
+    print(f'Part_1: {data}')
+
+def part_2(data):
+    print(f'Part_2: {data}')
